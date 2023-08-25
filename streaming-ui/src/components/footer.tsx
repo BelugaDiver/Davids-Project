d6ab8d5bbed29ee6e8c5b244b868912f8b80240e
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2023-08-24 21:16:08 -0500

    finish footer

--- streaming-ui/src/components/footer.tsx
+++ streaming-ui/src/components/footer.tsx
@@ -2,8 +2,8 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export default function Footer() {
    return (
-      <footer role="footer" className="flex lg:items-center lg:justify-center py-20 md:p-20 w-full footer border-t md:px-36 text-sm">
-         <div className="lg:w-2/3 text-center md:text-left">
+      <footer role="footer" className="flex items-center justify-center py-20 md:p-20 w-full footer border-t md:px-36 text-sm">
+         <div className="md:w-2/3 max-w-screen-lg lg:px-2 text-center md:text-left">
             <p>Do not sell or share my info.</p>
             <br />
             <p className="opacity-60 flex items-center"><CopyrightIcon />&nbsp;Copyright, 2023 - David Asubiojo</p>
